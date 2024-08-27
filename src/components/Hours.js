ba77cca0b180c29dca5c92d5b6df91c08a53eafd
René <diaz.de.sandi.7@gmail.com>
2024-08-27 18:25:28 +0000

    Reto02

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,17 @@
+import React from 'react';
+
 // Placeholder to host a component
+const Hours = () => {
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDay = new Date().getDay();
+    const currentHours = new Date().getHours();
+
+    return (
+        <div>
+            <h2>Hours for {daysOfWeek[currentDay]}</h2>
+            <p>{currentHours} hours</p>
+        </div>
+    );
+};
+
+export default Hours;
